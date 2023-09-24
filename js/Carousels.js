68234cc36047e0d4ba2671dc8f0c968ae7c6f1d8
Inna-decsgr <anjgkwl27@gmail.com>
2023-09-24 13:02:46 +0900

    🚧 Separate .user-gallery :mobile/desktop

--- js/Carousels.js
+++ js/Carousels.js
@@ -11,11 +11,10 @@ const productCarousel = tns({
   preventScrollOnTouch: true,
 })
 
-const userGallery = tns({
-  container: '.user-gallery .slider-list',
+const userGalleryMobile = tns({
+  container: '.user-gallery.is-mobile .slider-list',
   controls: false,
-  controlsContainer : ".user-gallery-controls",
-  navContainer: '.user-gallery .thumbnail-list',
+  navContainer: '.user-gallery.is-mobile .thumbnail-list',
   navAsThumbnails: true,
   preventScrollOnTouch: true,
   arrowKeys: true,
@@ -23,12 +22,18 @@ const userGallery = tns({
   edgePadding: 16,
   mouseDrag: true,
   loop: false,
+})
 
-  responsive: {
-    768: {
-      controls: true,
-      gutter: 6,
-      edgePadding: 75,
-    },
-  },
+const userGalleryDesktop = tns({
+  container: '.user-gallery.is-desktop .slider-list',
+  controls: true,
+  controlsContainer: '.user-gallery.is-desktop .user-gallery-controls',
+  navContainer: '.user-gallery.is-desktop .thumbnail-list',
+  navAsThumbnails: true,
+  preventScrollOnTouch: true,
+  arrowKeys: true,
+  gutter: 6,
+  edgePadding: 75,
+  mouseDrag: true,
+  loop: false,
 })
