3c6fed5554551b9a1e957eacd0e3a71873158016
Inna-decsgr <anjgkwl27@gmail.com>
2023-09-24 10:36:16 +0900

    🚧 Initiate a new carousel : user-gallery

---
+++ js/Carousels.js
@@ -0,0 +1,31 @@
+const productCarousel = tns({
+  container: '.product-carousel .slider-list',
+  controls: false,
+  navContainer: '.product-carousel .thumbnail-list',
+  navAsThumbnails: true,
+  arrowKeys: true,
+  autoplay: true,
+  autoplayHoverPause: true,
+  autoplayButtonOutput: false,
+  mouseDrag: true,
+  preventScrollOnTouch: true,
+})
+
+const userGallery = tns({
+  container: '.user-gallery .slider-list',
+  controls: false,
+  preventScrollOnTouch: true,
+  arrowKeys: true,
+  gutter: 4,
+  edgePadding: 16,
+  mouseDrag: true,
+  loop: false,
+
+  responsive: {
+    768: {
+      controls: true,
+      gutter: 6,
+      edgePadding: 75,
+    },
+  },
+})
