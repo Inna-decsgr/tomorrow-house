6a7c18305f9e580e8a6501e6cf4df69f6548dfea
Inna-decsgr <anjgkwl27@gmail.com>
2023-09-30 12:57:16 +0900

    🚧 Implement deleting individual search-history-item

--- js/gnb-search.js
+++ js/gnb-search.js
@@ -3,34 +3,54 @@ const gnbSearchInput = gnbSearch.querySelector('input')
 const gnbSearchHistory = gnbSearch.querySelector('.search-history')
 const gnbSearchHistoryList = gnbSearchHistory.querySelector('ol')
 
-const deleteAllButton = gnbSearchHistory.querySelector('.search-history-header button')
+const deleteAllButton = gnbSearchHistory.querySelector(
+  '.search-history-header button'
+)
 
+const deleteButtonList = gnbSearchHistoryList.querySelectorAll('.delete-button')
+
+function closeGnbSearchHistoryClickingOutside() {
+  gnbSearchHistory.classList.remove('is-active')
+  window.removeEventListener('click', closeGnbSearchHistory)
+}
 
 function closeGnbSearchHistory(e) {
   if (!gnbSearch.contains(e.target)) {
-    gnbSearchHistory.classList.remove('is-active');
-    window.removeEventListener('click', closeGnbSearchHistory);
+    closeGnbSearchHistoryClickingOutside();
   }
 }
 
 function openGnbSearchHistory() {
-  if(gnbSearchHistoryList.children.length === 0) {
+  if (gnbSearchHistoryList.children.length === 0) {
     return
   }
 
-  if(!gnbSearchHistory.classList.contains('.is-active')){
-    window.addEventListener('click',closeGnbSearchHistory);
+  if (!gnbSearchHistory.classList.contains('.is-active')) {
+    window.addEventListener('click', closeGnbSearchHistory)
   }
-  
+
   gnbSearchHistory.classList.add('is-active')
 }
 
 gnbSearchInput.addEventListener('focus', openGnbSearchHistory)
 
 function deleteAllSearchHistoryItems() {
-  gnbSearchHistoryList.innerHTML = '';
-  gnbSearchHistory.classList.remove('is-active');
+  gnbSearchHistoryList.innerHTML = ''
+  closeGnbSearchHistoryClickingOutside();
 }
 
 deleteAllButton.addEventListener('click', deleteAllSearchHistoryItems)
 
+function deleteSearchHistoryItem(e) {
+  e.stopPropagation()
+  const itemToDelete = this.parentNode
+  gnbSearchHistoryList.removeChild(itemToDelete)
+
+  if (gnbSearchHistoryList.children.length === 0) {
+    closeGnbSearchHistoryClickingOutside();
+  }
+}
+
+deleteButtonList.forEach((button) => {
+  button.addEventListener('click', deleteSearchHistoryItem)
+})
